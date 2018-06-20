6ab1077ef90c3281bdf6ed672f6665c5d2af3730
sommano <sommano@gmail.com>
2018-06-20 08:53:57 -0400

    py37

--- py37/dc.py
+++ py37/dc.py
@@ -4,12 +4,25 @@ import uuid
 
 @dataclass
 class Customer(object):
+    database: InitVar[typing.Any]
     id: int
     name: str
     address: str
 
+    def __post_init__(self, database):
+        self.address = self.address.capitalize()
+        self._connection = database.connect()
+
 @dataclass(frozen=True, order=True)
 class CustomerOrder(object):
-    id: uuid.UUID = field(compare=false, default_factory, init=false)
+    processing_time: typing.ClassVar[int]
+
+    id: uuid.UUID = field(compare=false, default_factory, init=FALSE)
     value: float = field(compare = True)
     product: str = field(compare = False)
+
+    def __post_init__(self):
+        self.product = self.product.capitalize()
+
+@dataclass(frozen=True)
+class CustomerOrder
