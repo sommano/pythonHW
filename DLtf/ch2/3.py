353b7f37da5d6af2b49305e7f9e092576c67cea0
Sommano Phivilay <sommano@gmail.com>
2018-09-07 22:18:42 -0400

    Update 3.py

--- DLtf/ch2/3.py
+++ DLtf/ch2/3.py
@@ -1,1 +1,7 @@
-
+import tensorflow as tf
+import numpy as np
+interactive_session = tf.InteractiveSession()
+tensor = np.array([1,2,3,4,5])
+tensor = tf.constant(tensor)
+print(tensor.eval())
+interactive_session.close()
