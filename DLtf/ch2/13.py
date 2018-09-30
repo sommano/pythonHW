c489e02effb4dec33a1a35b5f83f3062a578f6eb
Sommano Phivilay <sommano@gmail.com>
2018-09-29 21:12:36 -0400

    Update 13.py

--- DLtf/ch2/13.py
+++ DLtf/ch2/13.py
@@ -1,5 +1,18 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 learning_rate = 0.001
+
+training_iters = 100000
+batch_size = 128
+display_step = 10
+
+n_input = 28
+n_steps = 28
+n_hidden = 128
+n_classes = 10
+
+x = tf.placeholder("float", [None, n_steps, n_input])
+y = tf.placeholder("float", [None, n_classes])
