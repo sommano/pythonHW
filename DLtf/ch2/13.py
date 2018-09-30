5d0312b5a0d761e618926d6bad655bf012c482f2
Sommano Phivilay <sommano@gmail.com>
2018-09-29 21:05:11 -0400

    Update 13.py

--- DLtf/ch2/13.py
+++ DLtf/ch2/13.py
@@ -1,19 +1,5 @@
-import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+from tensorflow.contrib import rnn
 from tensorflow.examples.tutorials.mnist import input_data
-#Plot function
-def plotresult(org_vec,noisy_vec,out_vec):
- plt.matshow(np.reshape(org_vec, (28, 28)),\
- cmap=plt.get_cmap('gray'))
- plt.title("Original Image")
- plt.colorbar()
- plt.matshow(np.reshape(noisy_vec, (28, 28)),\
- cmap=plt.get_cmap('gray'))
- plt.title("Input Image")
- plt.colorbar()
- outimg = np.reshape(out_vec, (28, 28))\
- plt.matshow(outimg, cmap=plt.get_cmap('gray'))
- plt.title("Reconstructed Image")
- plt.colorbar()
- plt.show()
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+learning_rate = 0.001
